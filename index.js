a0a44e5f4b260c2b7fa2866f4d15d46480e95699
Robert Baglow <bagl0014@algonquinlive.com>
2024-06-21 22:26:06 -0400

    added welcome

--- index.js
+++ index.js
@@ -9,14 +9,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB
+// Connect to MongoDB Atlas
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
-    console.log('Connected to MongoDB');
+    console.log('Connected to MongoDB Atlas');
 }).catch(err => {
-    console.error('Failed to connect to MongoDB', err);
+    console.error('Failed to connect to MongoDB Atlas', err);
+});
+
+// Welcome route
+app.get('/', (req, res) => {
+    res.send('Welcome to My Online Grocery!');
 });
 
 // Routes
